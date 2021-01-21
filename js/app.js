3fc406943b101e79127365623350f45377d488ab
Vlggomes <74120529+Vlggomes@users.noreply.github.com>
2021-01-20 16:33:07 -0800

    initial commit

---
+++ js/app.js
@@ -0,0 +1,13 @@
+      var a = 2000;
+      var b = 150;
+      var c = a-b;
+      var d = a+b;
+      var e = a*b;
+
+      console.log(a)
+      console.log(b)
+      console.log(c)
+      console.log(d)
+      console.log(e)
+      window.alert(e)
+      window.alert('Welcome to our 1st day class');
